8f122a4bdda978aa5201e65db0ca5acad4fb16de
AshwinkumarV-havi <72845515+AshwinkumarV-havi@users.noreply.github.com>
2020-10-14 12:46:42 +0530

    Delete vconnect.php

--- vconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host='54.84.152.81';
-$db_user='root';
-$db_passwd='root';
-$db_name='votedb';
-$con = mysqli_connect($db_host,$db_user,$db_passwd,$db_name) or die("Error connecting to the database");
-?>
