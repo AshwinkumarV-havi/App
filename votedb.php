c678c0b9b02e082942a626db4ae9fcb4a9227b2f
AshwinkumarV-havi <72845515+AshwinkumarV-havi@users.noreply.github.com>
2020-10-14 12:04:10 +0530

    Update votedb.php

--- votedb.php
+++ votedb.php
@@ -1,7 +1,7 @@
 <?php
-$db_host='localhost';
+$db_host='54.84.152.81';
 $db_user='root';
-$db_passwd='';
+$db_passwd='root';
 $db_name='votedb';
 $dbh = mysqli_connect($db_host,$db_user,$db_passwd,$db_name) or die("Error connecting to the database");
 echo"connection is success";
